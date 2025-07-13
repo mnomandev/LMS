fc302f492b07bfb4b72ecfe2a6118d8af1d10ba2
nomandev <nomankhan02432@gmail.com>
2025-07-13 21:55:55 +0500

    Homepage halfway

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CoursesSection = () => {
   return (
-    <div>
-      <h1>Courses Section</h1>
+    <div className='py-16 md:px-40 px-8'>
+     <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+     <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to 
+      bussiness and wellness, our courses are craft to deliver results.</p>
+
+
+      <Link to={"/course-list"} onClick={() => scroll(0,0)} className='text-gray-500 border
+      border-gray-500/30 px-10 py-3 rounded'>Show all c ourses</Link>
     </div>
   )
 }
