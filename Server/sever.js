7b224992668862419a6b3cf903288d6b2fb79db7
nomandev <nomankhan02432@gmail.com>
2025-07-30 16:06:53 +0500

    changes back

---
+++ Server/sever.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import connectDB  from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
+
+
+//initialize express app
+const app = express();
+
+await connectDB();
+
+//middleware
+app.use(cors())
+
+//Routes
+app.get('/', (req, res) => res.send('API is running'));
+app.post('/clerk', express.json(), clerkWebhooks)
+
+
+//port
+const PORT = process.env.PORT || 5000;
+
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    
+});
