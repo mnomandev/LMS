868548cca09627949b224dd1ae2caf095bffbb0c
nomandev <nomankhan02432@gmail.com>
2025-09-08 17:02:33 +0500

    testing with email

--- Server/models/User.js
+++ Server/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: false,
+        required: true,
         unique: true
     },
     imageUrl: {
