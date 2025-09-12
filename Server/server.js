aa9934b59957148fb6e8c5593e23173e7341a165
nomandev <nomankhan02432@gmail.com>
2025-09-12 17:53:04 +0500

    clerkMiddleware configured

--- Server/server.js
+++ Server/server.js
@@ -22,7 +22,10 @@ const app = express();
 
 // CORS middleware
 app.use(cors());
-app.use(clerkMiddleware());
+app.use(clerkMiddleware({
+    publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    secretKey: process.env.CLERK_SECRET_KEY,
+  }));
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
